848d30678fdf40e5ac269f62ac2e7cbf352866c1
Evgeniya Kotova <zheniyasl@gmail.com>
2021-06-14 16:56:50 +0200

    add AppTitle react component, move code from ListBooks.js to this component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 // import * as BooksAPI from './BooksAPI'
 import "./App.css";
-import ListBooks from "ListBooks";
+import ListBooks from "./ListBooks";
 
 class BooksApp extends React.Component {
   state = {
--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
+import AppTitle from "./ListBookTitle";
 
 class ListBooks extends Component {
   render() {
     return (
       <div className="list-books">
-        <div className="list-books-title">
-          <h1>MyReads</h1>
-        </div>
+        <AppTitle />
         <div className="list-books-content">
           <div>
             <div className="bookshelf">
