84a9a9b877be1e4a7dbed71068dddf0ae77a0a0d
Evgeniya Kotova <zheniyasl@gmail.com>
2021-06-16 15:49:38 +0200

    add Book react component

---
+++ src/Book.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+
+class Book extends Component {
+  render() {
+    const book = this.props.book;
+    return (
+      <div className="book">
+        <div className="book-top">
+          <div
+            className="book-cover"
+            style={{
+              width: 128,
+              height: 193,
+              backgroundImage: book.imageLinks.smallThumbnail,
+            }}
+          />
+          <div className="book-shelf-changer">
+            <select>
+              <option value="move" disabled>
+                Move to...
+              </option>
+              <option value="currentlyReading">Currently Reading</option>
+              <option value="wantToRead">Want to Read</option>
+              <option value="read">Read</option>
+              <option value="none">None</option>
+            </select>
+          </div>
+        </div>
+        <div className="book-title">To Kill a Mockingbird</div>
+        <div className="book-authors">Harper Lee</div>
+      </div>
+    );
+  }
+}
+export default Book;
--- src/BookshelfBooks.js
+++ src/BookshelfBooks.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Book from "./Book";
 
 class BookshelfBooks extends Component {
   render() {
@@ -9,33 +10,7 @@ class BookshelfBooks extends Component {
             .filter((book) => book.shelf === this.props.shelf)
             .map((book) => (
               <li key={book.id}>
-                <div className="book">
-                  <div className="book-top">
-                    <div
-                      className="book-cover"
-                      style={{
-                        width: 128,
-                        height: 193,
-                        backgroundImage: book.imageLinks.smallThumbnail,
-                      }}
-                    />
-                    <div className="book-shelf-changer">
-                      <select>
-                        <option value="move" disabled>
-                          Move to...
-                        </option>
-                        <option value="currentlyReading">
-                          Currently Reading
-                        </option>
-                        <option value="wantToRead">Want to Read</option>
-                        <option value="read">Read</option>
-                        <option value="none">None</option>
-                      </select>
-                    </div>
-                  </div>
-                  <div className="book-title">To Kill a Mockingbird</div>
-                  <div className="book-authors">Harper Lee</div>
-                </div>
+                <Book book={book} />
               </li>
             ))}
         </ol>
