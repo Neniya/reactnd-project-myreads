6947ee952994807c4644f04d3f4e1cd8e17a91cf
Evgeniya Kotova <zheniyasl@gmail.com>
2021-06-17 16:33:35 +0200

    add SearchBook component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import * as BooksAPI from "./BooksAPI";
 import "./App.css";
 import ListBooks from "./ListBooks";
 import { Route, Link } from "react-router-dom";
-import BooksGrid from "./Booksgrid";
+
+import SearchBook from "./SearchBook";
 
 class BooksApp extends React.Component {
   state = {
@@ -22,29 +23,7 @@ class BooksApp extends React.Component {
       <div className="app">
         <Route
           path="/search"
-          render={() => (
-            <div className="search-books">
-              <div className="search-books-bar">
-                <Link to="/">
-                  <button className="close-search">Close</button>
-                </Link>
-                <div className="search-books-input-wrapper">
-                  {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                  <input type="text" placeholder="Search by title or author" />
-                </div>
-              </div>
-              <div className="search-books-results">
-                <BooksGrid books={this.state.books} isShelf={false} />
-              </div>
-            </div>
-          )}
+          render={() => <SearchBook books={this.state.books} />}
         />
         <Route
           exact
