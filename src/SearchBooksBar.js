90bccf3d5db32feeb47f37b330e2317eabc67e96
Evgeniya Kotova <zheniyasl@gmail.com>
2021-06-18 11:37:51 +0200

    add SearchBookInputWrapper and SearchBookBar react components

---
+++ src/SearchBooksBar.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import SearchBookInputWrapper from "./SearchBookInput";
+
+class SearchBookBar extends Component {
+  render() {
+    return (
+      <div className="search-books-bar">
+        <Link to="/">
+          <button className="close-search">Close</button>
+        </Link>
+        <SearchBookInputWrapper />
+      </div>
+    );
+  }
+}
+
+export default SearchBookBar;
