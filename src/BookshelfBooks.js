c3b85cb312b50a55a53cf934e84f870e4bba660c
Evgeniya Kotova <zheniyasl@gmail.com>
2021-06-23 14:52:27 +0200

    add cheking PropTypes to BookshelfBooks component

--- src/BookshelfBooks.js
+++ src/BookshelfBooks.js
@@ -1,7 +1,14 @@
 import React, { Component } from "react";
 import BooksGrid from "./Booksgrid";
+import PropTypes, { object } from "prop-types";
 
 class BookshelfBooks extends Component {
+  static propTypes = {
+    books: PropTypes.arrayOf(object),
+    updateBookShelf: PropTypes.func.isRequired,
+    shelf: PropTypes.string.isRequired,
+  };
+
   render() {
     return (
       <div className="bookshelf-books">
