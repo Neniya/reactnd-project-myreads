c845bcfc4b30f22373cd65066297da1c5ba1dc9d
Evgeniya Kotova <zheniyasl@gmail.com>
2021-06-23 14:41:42 +0200

    add cheking PropTypes to ListBooks component

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
 import AppTitle from "./ListBookTitle";
 import ListBooksContent from "./ListBooksContent";
+import PropTypes, { object } from "prop-types";
 
 class ListBooks extends Component {
+  static propTypes = {
+    books: PropTypes.arrayOf(object),
+    updateBookShelf: PropTypes.func.isRequired,
+  };
+
   render() {
     return (
       <div className="list-books">
