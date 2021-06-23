8ae34ec5e383ef8cfe1b3f37ab4887cefaa3720a
Evgeniya Kotova <zheniyasl@gmail.com>
2021-06-23 14:33:35 +0200

    add cheking PropTypes to SearchBooksBar component

--- src/SearchBooksBar.js
+++ src/SearchBooksBar.js
@@ -1,8 +1,13 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import SearchBookInputWrapper from "./SearchBookInput";
+import PropTypes from "prop-types";
 
 class SearchBookBar extends Component {
+  static propTypes = {
+    searchBooks: PropTypes.func.isRequired,
+  };
+
   render() {
     return (
       <div className="search-books-bar">
