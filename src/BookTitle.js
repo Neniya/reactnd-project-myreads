69c8aa8ea74b113715bf9762ab95b90a248e4526
Evgeniya Kotova <zheniyasl@gmail.com>
2021-06-23 15:05:26 +0200

    add cheking PropTypes to BookTitle component

--- src/BookTitle.js
+++ src/BookTitle.js
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 class BookTitle extends Component {
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+  };
+
   render() {
     return <div className="book-title">{this.props.title}</div>;
   }
